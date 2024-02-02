92304e411ebf048e897f6685b753b100e44b06cd
eunsil Yu <84001448+eunsilyu@users.noreply.github.com>
2024-02-02 15:20:58 +0900

    Update image.js

--- js/image.js
+++ js/image.js
@@ -4,7 +4,7 @@ const images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg", "07.
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const putImgae = document.createElement("img");
-putImgae.src = `./img/${chosenImage}`;
+putImgae.src = `img/${chosenImage}`;
 putImgae.classList.add("randomimg")
 
 const imgSection = document.querySelector("#wrap");
